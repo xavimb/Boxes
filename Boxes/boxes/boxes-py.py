97886fa90cc4601f9be5ce239031a80cd0521ad7
Xavi Magrinyà <xavi.mb89@hotmail.com>
2013-04-23 21:43:20 +0200

    file name changed main function

--- Boxes/boxes/boxes-py.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Apr 23, 2013
-
-@author: xavier
-'''
-
-if __name__ == '__main__':
-    pass
---
+++ Boxes/boxes/boxes.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    print "holahola"
+    return 0
+
+
+if __name__ == "__main__":
+    main()
