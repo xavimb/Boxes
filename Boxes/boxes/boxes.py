be6c2ac1be59a3849b2f4a54ee98ab01aa85d136
Xavi Magrinyà <xavi.mb89@gmail.com>
2013-05-11 15:40:16 +0200

    Done

--- Boxes/boxes/boxes.py
+++ Boxes/boxes/boxes.py
@@ -1,3 +1,4 @@
+import sys
 
 boxes = []
 tam = []
@@ -5,11 +6,14 @@ n_boxes = 0
 
 def ind_pos(i, j, k):
     global tam
-    return str((i*tam[0]*tam[1]) + (j*tam[1]) + k + 1)
+    global n_boxes
+    return str((i*n_boxes) + (j*tam[0]*n_boxes) + k + 1)
+    #return str(i) + str(j) + str(k)
 
 def main():
-    f = open('file.txt')
+    f = open(sys.argv[1])
     global tam
+    global n_boxes
     tam = f.readline().split(" ")
     tam[0] = int(tam[0])
     tam[1] = int(tam[1])
@@ -21,30 +25,88 @@ def main():
         else:
             print "ERROR PARSING"
         
-    print "p cnf %d %d" % (tam[0]*tam[1]*n_boxes + tam[0]*tam[1]*n_boxes, tam[0]*tam[1])
-    #minimo una caja por celda
-    #para cada caja i,j -> 0 v 1 v ... v k
-    for i in xrange(0,tam[0]):
-        for j in xrange(0, tam[1]):
-            for k in xrange(0, n_boxes):
-                print ind_pos(i,j,k),
-            print "0"
+    count = 0;
     
+     #todas las cajas tienen alguna posicion ----- minimo ------
+    for k in xrange(0,n_boxes):
+        count += 1
+    
+    #maximo una caja esta en un sitio ----- maximo -------
+    if (n_boxes >= 2):
+        for k in xrange(0, n_boxes):
+            for i in xrange(0, tam[0]-boxes[k][0]+1):
+                for j in xrange(0, tam[1]-boxes[k][1]+1):
+                    for l in xrange(0, tam[0]-boxes[k][0]+1):
+                        for m in xrange(0, tam[1]-boxes[k][1]+1):
+                            if (not (l == i and m == j)):
+                                count += 1
+                                
     #maximo una caja por celda
     for i in xrange(0, tam[0]):
-        for j in xrange(0, tam[1]):
+        for j in xrange(0,tam[1]):
             for k in xrange(0, n_boxes):
-                print "-" + ind_pos(i,j,k) + " ",
-            print "0"
+                for l in xrange(0, n_boxes):
+                    if (k != l):
+                        count += 1
     
-    dnfs = []
+    #si una esquina esta en un sitio, no esta en ningun otro sitio 
     for k in xrange(0,n_boxes):
         for i in xrange(0, tam[0]-boxes[k][0]+1):
             for j in xrange(0, tam[1]-boxes[k][1]+1):
-                for l in xrange(0, boxes[k][0]):
-                    for m in xrange(0, boxes[k][1]):
-                        dnfs[k][i+j*tam[0]].append([ind_pos(i+l,j+m,k)])
-                print "0"
+                for m in xrange(0,boxes[k][0]):
+                    for n in xrange(0,boxes[k][1]):
+                        if (not (m == 0 and n == 0)):
+                            for l in xrange(0,n_boxes):
+                                if (l != k):
+                                    count += 1
+        
+    
+    print "p cnf %d %d" % (tam[0]*tam[1]*n_boxes, count)
+    
+    #todas las cajas tienen alguna posicion ----- minimo ------
+    for k in xrange(0,n_boxes):
+        for i in xrange(0, tam[0]-boxes[k][0]+1):
+            for j in xrange(0,tam[1]-boxes[k][1]+1):
+                print ind_pos(i,j,k) + " ",
+        print "0"
+    
+    #maximo una caja esta en un sitio ----- maximo -------
+    if (n_boxes >= 2):
+        for k in xrange(0, n_boxes):
+            for i in xrange(0, tam[0]-boxes[k][0]+1):
+                for j in xrange(0, tam[1]-boxes[k][1]+1):
+                    for l in xrange(0, tam[0]-boxes[k][0]+1):
+                        for m in xrange(0, tam[1]-boxes[k][1]+1):
+                            if (not (l == i and m == j)):
+                                print "-" + ind_pos(i,j,k) + " -" + ind_pos(l,m,k) + " 0"
+                                #print "pos:" + str(i) + str(j) + str(k) + " pos2:" + str(l) + str(m) +str(k)
+
+    #maximo una caja por celda
+    for i in xrange(0, tam[0]):
+        for j in xrange(0,tam[1]):
+            for k in xrange(0, n_boxes):
+                for l in xrange(0, n_boxes):
+                    if (k != l):
+                        print "-" + ind_pos(i,j,k) + " -" + ind_pos(i,j,l) + " 0"
+                        #print "pos:" + str(i) + str(j) + str(k) + " pos2:" + str(i) + str(j) +str(l)
+    
+    #si una esquina esta en un sitio, no esta en ningun otro sitio 
+    for k in xrange(0,n_boxes):
+        for i in xrange(0, tam[0]-boxes[k][0]+1):
+            for j in xrange(0, tam[1]-boxes[k][1]+1):
+                for m in xrange(0,boxes[k][0]):
+                    for n in xrange(0,boxes[k][1]):
+                        if (not (m == 0 and n == 0)):
+                            for l in xrange(0,n_boxes):
+                                if (l != k):
+                                    print "-" + ind_pos(i,j,k) + " -" + ind_pos(i+m,j+n,l) + " 0"
+                                    #print "pos:" + str(i) + str(j) + str(k) + " pos2:" + str(m) + str(n) +str(l)
+                                    # falta bucle para cada caja, dejar margen
+                                    for p in xrange(0,boxes[l][0]):
+                                        for q in xrange(0, boxes[l][1]):
+                                            if (i+m-p >= 0 and i+m-p < tam[0] and j+n-q >= 0 and j+n-q < tam[1]):
+                                                print "-" + ind_pos(i,j,k) + " -" + ind_pos(i+m-p,j+n-q,l) + " 0"
+    
     f.close()
     return 0
 
